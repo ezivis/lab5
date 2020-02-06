606c7cc64c77086b57977c3b0d0c12d4794785e2
ezivis <49287721+ezivis@users.noreply.github.com>
2020-02-06 10:39:01 -0800

    Update hci-friends.js

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$(".friend").click(function(e) {
+		e.preventDefault();
+		var name = $(this).text();
+		$(this).text(anagrammedName(name));
+	});
 }
 
 function anagrammedName(name) {
